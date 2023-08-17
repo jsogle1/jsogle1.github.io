874cc38e2b6080ffc6f3d0cf9ab72e36049688c1
jsogle1 <105501317+jsogle1@users.noreply.github.com>
2023-08-17 12:08:00 -0400

    Update story_map_logic.js
    Fetch placed into it.

--- story_map_logic.js
+++ story_map_logic.js
@@ -5,48 +5,61 @@ const basemaps = {
     Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?', { layers: 'TOPO-WMS' }),
     Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', { layers: 'OSM-Overlay-WMS' })
 };
+
 L.control.layers(basemaps).addTo(map);
 basemaps.Topography.addTo(map);
 
-var storyData = L.geoJSON(GeoJSONData).addTo(map);
+// Fetch the GeoJSON file and then process it
+fetch('Henry_V_Leaflet.geojson')
+    .then(response => response.json())
+    .then(data => {
+        var storyData = L.geoJSON(data).addTo(map);
 
-storyData.forEach((storyPoint, index) => {
-    var storyDiv = document.createElement('div');
-    storyDiv.className = 'story-section';
-    storyDiv.dataset.index = index;
+        storyData.eachLayer((layer) => {
+            var storyPoint = layer.feature.properties;
+            var storyDiv = document.createElement('div');
+            storyDiv.className = 'story-section';
+            storyDiv.dataset.index = layer._leaflet_id - 1;
 
-    if (storyPoint.properties.title) {
-        var titleElement = document.createElement('h2');
-        titleElement.textContent = storyPoint.properties.title;
-        storyDiv.appendChild(titleElement);
-    }
+            if (storyPoint.title) {
+                var titleElement = document.createElement('h2');
+                titleElement.textContent = storyPoint.title;
+                storyDiv.appendChild(titleElement);
+            }
 
-    var contentElement = document.createElement('p');
-    contentElement.innerHTML = storyPoint.properties.content;
-    storyDiv.appendChild(contentElement);
+            var contentElement = document.createElement('p');
+            contentElement.innerHTML = storyPoint.content;
+            storyDiv.appendChild(contentElement);
 
-    if (storyPoint.properties.image && storyPoint.properties.image.trim() !== "") {
-        var imgElement = document.createElement('img');
-        imgElement.src = storyPoint.properties.image;
-        imgElement.alt = "Image for " + storyPoint.properties.content;
-        storyDiv.appendChild(imgElement);
-    }
-    
-    document.getElementById('story-details').appendChild(storyDiv);
-});
+            if (storyPoint.image && storyPoint.image.trim() !== "") {
+                var imgElement = document.createElement('img');
+                imgElement.src = storyPoint.image;
+                imgElement.alt = "Image for " + storyPoint.content;
+                storyDiv.appendChild(imgElement);
+            }
 
-document.getElementById('story-details').addEventListener('scroll', function(e) {
-    var storySections = document.querySelectorAll('.story-section');
-    var currentStoryIndex;
+            document.getElementById('story-details').appendChild(storyDiv);
+        });
 
-    storySections.forEach((section, index) => {
-        var rect = section.getBoundingClientRect();
-        if (rect.top >= 0 && rect.top <= rect.height) {
-            currentStoryIndex = index;
-        }
+        document.getElementById('story-details').addEventListener('scroll', function(e) {
+            var storySections = document.querySelectorAll('.story-section');
+            var currentStoryIndex;
+
+            storySections.forEach((section, index) => {
+                var rect = section.getBoundingClientRect();
+                if (rect.top >= 0 && rect.top <= rect.height) {
+                    currentStoryIndex = index;
+                }
+            });
+
+            if (typeof currentStoryIndex !== "undefined") {
+                var layer = storyData.getLayer(currentStoryIndex + 1);
+                if (layer) {
+                    map.flyTo(layer.getLatLng(), layer.feature.properties.zoom);
+                }
+            }
+        });
+    })
+    .catch(error => {
+        console.error("Error fetching the GeoJSON data: ", error);
     });
-
-    if (typeof currentStoryIndex !== "undefined" && storyData[currentStoryIndex]) {
-        map.flyTo([storyData[currentStoryIndex].geometry.coordinates[1], storyData[currentStoryIndex].geometry.coordinates[0]], storyData[currentStoryIndex].properties.zoom);
-    }
-});
