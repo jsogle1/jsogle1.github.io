d84d9e8dbb1abdefebc95ac362b2ccdd193a944a
jsogle1 <105501317+jsogle1@users.noreply.github.com>
2023-08-17 13:41:39 -0400

    Update story_map_logic.js
    fetched differently

--- story_map_logic.js
+++ story_map_logic.js
@@ -1,71 +1,58 @@
 var map = L.map('map').setView([50,0], 5);
 
 const basemaps = {
-    StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }),
-    Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {
-        layers: 'TOPO-WMS'
-    }),
-    Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {
-        layers: 'OSM-Overlay-WMS'
-    })
+    StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '© OpenStreetMap contributors'}),
+    Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'TOPO-WMS'}),
+    Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'})
 };
 L.control.layers(basemaps).addTo(map);
 basemaps.Topography.addTo(map);
 
-// Fetch GeoJSON
 fetch('Henry_V_Leaflet.geojson')
 .then(response => response.json())
 .then(data => {
-    var storyData = L.geoJSON(data, {
-        onEachFeature: function(feature, layer) {
-            // For each point in the GeoJSON, create a corresponding story section
-            var storyDiv = document.createElement('div');
-            storyDiv.className = 'story-section';
+    var storyData = data.features;
 
-            if (feature.properties.title) {
-                var titleElement = document.createElement('h2');
-                titleElement.textContent = feature.properties.title;
-                storyDiv.appendChild(titleElement);
-            }
+    storyData.forEach((storyPoint, index) => {
+        var storyDiv = document.createElement('div');
+        storyDiv.className = 'story-section';
+        storyDiv.dataset.index = index;
 
-            var contentElement = document.createElement('p');
-            contentElement.innerHTML = feature.properties.content;
-            storyDiv.appendChild(contentElement);
+        var props = storyPoint.properties;
 
-            if (feature.properties.image && feature.properties.image.trim() !== "") {
-                var imgElement = document.createElement('img');
-                imgElement.src = feature.properties.image;
-                imgElement.alt = "Image for " + feature.properties.content;
-                storyDiv.appendChild(imgElement);
-            }
-
-            document.getElementById('story-details').appendChild(storyDiv);
+        if (props.title) {
+            var titleElement = document.createElement('h2');
+            titleElement.textContent = props.title;
+            storyDiv.appendChild(titleElement);
         }
-    }).addTo(map);
+        
+        var contentElement = document.createElement('p');
+        contentElement.innerHTML = props.content;
+        storyDiv.appendChild(contentElement);
 
-    // Adjusted scroll event listener:
+        if (props.image && props.image.trim() !== "") {
+            var imgElement = document.createElement('img');
+            imgElement.src = props.image;
+            imgElement.alt = "Image for " + props.content;
+            storyDiv.appendChild(imgElement);
+        }
+        
+        document.getElementById('story-details').appendChild(storyDiv);
+    });
+
     document.getElementById('story-details').addEventListener('scroll', function(e) {
-        var storySections = document.querySelectorAll('.story-section');
-        var currentStoryIndex;
+        var top = e.target.scrollTop;
+        var height = e.target.clientHeight;
 
-        storySections.forEach((section, index) => {
-            var rect = section.getBoundingClientRect();
-            if (rect.top >= 0 && rect.top <= rect.height) {
-                currentStoryIndex = index;
-            }
-        });
+        var index = Math.floor(top / height);
 
-        if (typeof currentStoryIndex !== "undefined") {
-            var layer = storyData.getLayer(currentStoryIndex + 1); // The +1 is because Leaflet assigns IDs starting from 1
-            if (layer) {
-                var targetZoom = layer.feature.properties.zoom ? layer.feature.properties.zoom : map.getZoom();
-                map.flyTo(layer.getLatLng(), targetZoom);
-            }
+        var coords = storyData[index].geometry.coordinates;
+
+        if (storyData[index]) {
+            map.flyTo([coords[1], coords[0]], storyData[index].properties.zoom);
         }
     });
-})
-.catch(error => {
+
+}).catch(error => {
     console.error("There was an error fetching the GeoJSON:", error);
 });
