4c007a3d2edcebb4cd47cc57a46d9587d2358bd2
jsogle1 <105501317+jsogle1@users.noreply.github.com>
2023-08-17 21:33:28 -0400

    Update story_map_logic.js
    fixed error in streetview line

--- story_map_logic.js
+++ story_map_logic.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([50,0], 7);
 
 const basemaps = {
-    StreetView: Places: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+    StreetView: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }),
     Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'TOPO-WMS'}),
