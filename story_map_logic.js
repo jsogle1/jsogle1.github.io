f60453088bdedf345b542af8e35e22fbc0ea02f1
jsogle1 <105501317+jsogle1@users.noreply.github.com>
2023-08-17 21:54:00 -0400

    Update story_map_logic.js
    Changed Watercolor and Places

--- story_map_logic.js
+++ story_map_logic.js
@@ -1,11 +1,11 @@
 var map = L.map('map').setView([50,0], 7);
 
 const basemaps = {
-    StreetView: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+    Watercolor: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }),
     Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'TOPO-WMS'}),
-    Watercolor: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'})
+    Places: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'OSM-Overlay-WMS'})
 };
 L.control.layers(basemaps).addTo(map);
 basemaps.StreetView.addTo(map);
