3f3453b0b05ff700caf0902ce6880f64c894332d
jsogle1 <105501317+jsogle1@users.noreply.github.com>
2023-08-18 09:31:50 -0400

    Update story_map_logic.js
    reverted to last night - BEST WORKING VERSION

--- story_map_logic.js
+++ story_map_logic.js
@@ -1,96 +1,77 @@
-var map = L.map('map').setView([50, 0], 5);
+var map = L.map('map').setView([50,0], 5);
 
 const basemaps = {
-    StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap contributors' }),
-    Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?', { layers: 'TOPO-WMS' }),
-    Places: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', { attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.' })
+    StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '© OpenStreetMap contributors'}),
+    Topography: L.tileLayer.wms('http://ows.mundialis.de/services/service?', {layers: 'TOPO-WMS'}),
+    Places: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+    })
 };
+
 
 L.control.layers(basemaps).addTo(map);
 basemaps.Places.addTo(map);
 
 fetch('Henry_V_Leaflet.geojson')
-    .then(response => response.json())
-    .then(data => {
-        var storyData = data.features;
-        var markers = []; // Store all markers for reference
+.then(response => response.json())
+.then(data => {
+    var storyData = data.features;
 
-        storyData.forEach((storyPoint, index) => {
-            var navButton = document.createElement('div');
-            navButton.className = 'nav-section';
-            navButton.dataset.index = index;
-            navButton.textContent = storyPoint.properties.title ? storyPoint.properties.title : "Section " + (index + 1);
+    storyData.forEach((storyPoint, index) => {
+        var props = storyPoint.properties;
+        
+        var navSection = document.createElement('div');
+        navSection.className = 'nav-section';
 
-            var contentDiv = document.createElement('div');
-            contentDiv.className = 'content-section';
-            contentDiv.dataset.index = index;
-
-            if (storyPoint.properties.content) {
-                var contentElement = document.createElement('p');
-                contentElement.innerHTML = storyPoint.properties.content;
-                contentDiv.appendChild(contentElement);
+        // Populate the navigation
+        var navButton = document.createElement('button');
+        navButton.textContent = props.title;
+        navButton.onclick = function() {
+            var contentDiv = this.nextElementSibling;
+            var isVisible = contentDiv.style.display === 'block';
+            contentDiv.style.display = isVisible ? 'none' : 'block';
+            if(!isVisible) {
+                map.flyTo([storyPoint.geometry.coordinates[1], storyPoint.geometry.coordinates[0]], props.zoom);
             }
+        };
 
-            if (storyPoint.properties.image && storyPoint.properties.image.trim() !== "") {
-                var imgElement = document.createElement('img');
-                imgElement.src = storyPoint.properties.image;
-                imgElement.alt = storyPoint.properties.alt_text ? storyPoint.properties.alt_text : "Image for " + storyPoint.properties.title;
-                contentDiv.appendChild(imgElement);
+        navSection.appendChild(navButton);
 
-                if (storyPoint.properties.attribution) {
-                    var attributionElement = document.createElement('p');
-                    attributionElement.className = 'image-attribution';
-                    attributionElement.innerHTML = storyPoint.properties.attribution;
-                    contentDiv.appendChild(attributionElement);
-                }
-            }
+        // Populate the story content
+        var storyDiv = document.createElement('div');
+        storyDiv.className = 'story-content-section';
+        storyDiv.style.display = 'none';
 
-            console.log('Content section created for index:', index); // Debugging line
+        if (props.title) {
+            var titleElement = document.createElement('h2');
+            titleElement.textContent = props.title;
+            storyDiv.appendChild(titleElement);
+        }
+        
+        var contentElement = document.createElement('p');
+        contentElement.innerHTML = props.content;
+        storyDiv.appendChild(contentElement);
 
-            document.getElementById('story-nav').appendChild(navButton);
-            document.getElementById('story-content').appendChild(contentDiv);
+        if (props.image && props.image.trim() !== "") {
+            var imgElement = document.createElement('img');
+            imgElement.src = props.image;
+            imgElement.alt = "Image for " + props.content;
+            storyDiv.appendChild(imgElement);
+        }
+        
+        if (props.attribution) {
+            var attributionElement = document.createElement('p');
+            attributionElement.innerHTML = props.attribution;
+            storyDiv.appendChild(attributionElement);
+        }
 
-            // Add the point to the map
-            var coords = storyPoint.geometry.coordinates;
-            var marker = L.marker([coords[1], coords[0]]).addTo(map);
+        navSection.appendChild(storyDiv);
+        document.getElementById('story-details').appendChild(navSection);
+    });
 
-            // Bind title as popup (callout) to marker
-            if (storyPoint.properties.title) {
-                marker.bindPopup(storyPoint.properties.title);
-            }
-
-            // Store the marker
-            markers.push(marker);
-        });
-
-        document.getElementById('story-nav').addEventListener('click', function (e) {
-            console.log('Navigation button clicked'); // Debugging line
-
-            if (e.target && e.target.nodeName == "DIV") {
-                console.log('Trying to display content for index:', e.target.dataset.index); // Debugging line
-
-                var index = parseInt(e.target.dataset.index);
-                var coords = storyData[index].geometry.coordinates;
-
-                // Hide all content sections
-                var allContentSections = document.querySelectorAll('.content-section');
-                allContentSections.forEach(function(contentDiv) {
-                    contentDiv.style.display = 'none';
-                });
-
-                // Show the content section for the clicked nav section
-                var contentSectionToShow = document.querySelector('.content-section[data-index="' + index + '"]');
-                if (contentSectionToShow) {
-                    contentSectionToShow.style.display = 'block';
-                }
-
-                if (storyData[index]) {
-                    map.flyTo([coords[1], coords[0]], storyData[index].properties.zoom);
-                    markers[index].openPopup(); // Open the marker's popup (title callout)
-                }
-            }
-        });
-    })
+}).catch(error => {
+    console.error("There was an error fetching the GeoJSON:", error);
+});
     .catch(error => {
         console.error("There was an error fetching the GeoJSON:", error);
     });
